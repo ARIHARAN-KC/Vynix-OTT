5ad33a825e26ab114234fa22f9c440d8d5c486ac
ARIHARAN-KC <ariharan1718@gmail.com>
2025-10-11 14:33:21 +0530

    App.jsx fix

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -10,7 +10,7 @@ import Layout from "./components/Layout/Layout"
 import Login from "./components/Auth/Login/Login";
 import Signup from "./components/Auth/Signup/Signup";
 import OAuthSuccess from "./components/Auth/OAuthSuccess/OAuthSuccess";
-import AdminSignup from "./components/Auth/Admin/Signup";
+
 // Pages
 import Home from "./pages/Home/Home";
 import Browse from "./pages/Browser/Browse";
@@ -24,6 +24,12 @@ function App() {
       {/* Main content */}
       <main className="flex-1">
         <Routes>
+          {/* Admin templates */}
+          <Route path="/admin/Signup" element={
+            <Layout>
+              <AdminSignup/>
+            </Layout>
+          }/>
           {/* Pages without Layout (if you want Navbar+Footer on these) */}
           <Route path="/" element={
             <>
@@ -77,11 +83,6 @@ function App() {
               <OAuthSuccess />
             </Layout>
           } />
-          <Route path="/admin/Signup" element={
-            <Layout>
-              <AdminSignup/>
-            </Layout>
-          }/>
         </Routes>
       </main>
     </div>
