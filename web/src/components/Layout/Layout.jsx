b51135b2a7c981c23902742dff09940542387be3
ARIHARAN-KC <ariharan1718@gmail.com>
2025-10-11 14:44:28 +0530

    layout.jsx fix

--- web/src/components/Layout/Layout.jsx
+++ web/src/components/Layout/Layout.jsx
@@ -4,7 +4,6 @@ import Navbar from '../Navbar/Navbar';
 const Layout = ({ children }) => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#0B0B17] via-[#1A0B2E] to-[#2D0B45]">
-      <Navbar />
       <main className="pt-20">
         {children}
       </main>
